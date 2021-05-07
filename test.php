0a1b2b6cbc85ccdefd5ed535311e28e40b94a072
Yunicia <clairetourtoulou@gmail.com>
2021-05-07 15:55:50 +0200

    Mise à jour des classes personnages

---
+++ test.php
@@ -0,0 +1,21 @@
+<?php
+
+abstract class Arme{
+    protected static $nom;
+
+    public static function test(){
+        echo static::$nom.'<br/>';
+    }
+}
+
+class Baton extends Arme{
+    protected static $nom = "baton";
+
+}
+
+class Epee extends Arme{
+    protected static $nom = "epee";
+}
+
+Baton::test(); //affiche baton
+Epee::test(); //affiche epee
